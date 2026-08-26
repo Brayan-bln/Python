d333285fc4ace24e13213862a0b1c33ae11af2ec
Brayan-bln <brayin.bln@gmail.com>
2026-08-25 18:43:33 -0600

    tema-01: ejercicio 1-1 - print

---
+++ formacion/tema-01/a01.py
@@ -0,0 +1,22 @@
+#print = imprimir o mostrar en pantalla
+print("Hola Mundo", type("Hola Mundo"))
+print(10, type(10))
+
+"""
+Práctica Print 1
+Crea un código que imprima en pantalla la expresión Me encanta estudiar Python.
+"""
+print("Me encanta estudiar Python")
+
+"""
+Práctica Print 2
+Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+"""
+print("Estudiar con \"Python Total\" es super divertido")
+
+"""
+Práctica Print 3
+Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, 
+sino como resultado de una operación matemática
+"""
+print(500+50+5)
