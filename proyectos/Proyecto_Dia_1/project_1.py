dea14ed8e6176b1a9d6b2a9d98e5417e316cd27d
Brayan-bln <brayin.bln@gmail.com>
2026-08-31 16:13:20 -0600

    Subo contenido del proyecto del dia 1 de python

---
+++ proyectos/Proyecto_Dia_1/project_1.py
@@ -0,0 +1,11 @@
+"""
+Vas a crear un código en Python que le pida a tu amigo que responda dos preguntas que
+requieran una sola palabra cada una y que luego le muestre en pantalla esas palabras
+combinadas, para formar una marca creativa.
+"""
+print("\tEl nombre de tu ceverceria\n\tes \""
+      + input("Dime tu ciudad favorita: ")
+      + " "
+      + input("Dime tu color favorito: ")
+      + "\""
+      + "\n\tFelicidades!!!")
